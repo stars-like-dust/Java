ad3b11920cb223b29d9463c1ec63120260489f49
stars-like-dust <37813678+stars-like-dust@users.noreply.github.com>
2018-09-06 10:36:23 -0400

    Add files via upload

---
+++ JavaScriptHomework/static/js/app.js
@@ -0,0 +1,56 @@
+
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+
+//Put the tbody into a variable
+
+var tbody = d3.select('tbody');
+
+
+//loop that appends to the data to the console
+//data.forEach(function(theDate) {
+    // console.log(theDate);
+    //var row = tbody.append("tr");
+   // });
+
+// loop that appends to the console and the html
+data.forEach(function(theDate) {
+    console.log(theDate);
+    var row = tbody.append("tr");
+    Object.entries(theDate).forEach(function([key, value]) {
+    console.log(key, value);
+      // Append a cell to the row for each value
+      // in the weather report object
+    var cell = tbody.append("td");
+    cell.text(value);
+    });
+   });
+
+//Put filter bttn in a variabe
+var filterBttn = d3.select('#filter-btn');
+
+//Listen for the  'click' and initialize function
+
+filterBttn.on('click', function() {
+
+    // Disable default function of bttn
+    d3.filterBttn.preventDefault();
+
+    //Select user input 
+    var userInput = d3.slect('#datetime');
+
+    //Grab the user's input
+    var userInput2 = userInput.property('value');
+
+    //filter the data
+    var dates = data.filter(data => data.datetime === userInput2);
+
+    //display filtered data in console
+    console.log(dates)
+}
+)
+
+
+
